0ddba6a74e8d6afe4591a4f3280eb5af8edc8930
welefen <welefen@gmail.com>
2012-03-03 22:44:48 +0800

    update

--- src/html2json.js
+++ src/html2json.js
@@ -3,6 +3,7 @@
  * 
  * @author welefen
  * @lincense MIT
+ * @version 1.0 - 2012.03.02
  */
 this.html2json = (function() {
 	var Util = {
@@ -38,13 +39,19 @@ this.html2json = (function() {
 			return result;
 		}
 	}
-	var html2json = function(text) {
+	/**
+	 * [html2json]
+	 * @param  {[text]} text 
+	 * @param  {[function]} stringify
+	 * @return {[text]}
+	 */
+	var html2json = function(text, stringify) {
 		if (!Util.div) {
 			Util.div = document.createElement('div');
 		}
 		Util.div.innerHTML = text;
 		var result = Util.getJson(Util.div.childNodes);
-		return JSON.stringify(result);
+		return (window.JSON && JSON.stringify || stringify)(result);
 	}
 	return html2json;
 })()
